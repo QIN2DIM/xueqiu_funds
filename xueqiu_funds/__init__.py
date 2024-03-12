c8001011fb28d3759d6b27b3b43042fbaab76340
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2024-03-12 21:21:20 +0800

    add(examples): jupyter demo (#4)
    * add(examples): jupyter demo
    
    * fff

--- xueqiu_funds/__init__.py
+++ xueqiu_funds/__init__.py
@@ -3,8 +3,8 @@
 # Author     : QIN2DIM
 # GitHub     : https://github.com/QIN2DIM
 # Description:
+from .client import XueQiuFunds
 from .middleware.fund_detail import FundDetail
 from .middleware.fund_intro import FundIntro
-from .client import XueQiuFunds
 
 __all__ = ["FundDetail", "FundIntro", "XueQiuFunds"]
