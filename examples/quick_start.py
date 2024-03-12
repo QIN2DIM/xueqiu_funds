638e6ab336e71394f8af41f93f65ec74e06ba297
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2024-03-12 11:28:54 +0800

    v0.0.2 (#3)

--- examples/quick_start.py
+++ examples/quick_start.py
@@ -3,25 +3,17 @@
 # Author     : QIN2DIM
 # GitHub     : https://github.com/QIN2DIM
 # Description:
-import asyncio
-
 from xueqiu_funds import XueQiuFunds
 
+# 华夏上证科创板50成份ETF联接C
+fd_code = "011613"
 
-async def main():
-    # 华夏上证科创板50成份ETF联接C
-    fd_code = "011613"
+xqf = XueQiuFunds()
 
-    xqf = XueQiuFunds()
+detail = xqf.get_fund_detail(fd_code)
+intro = xqf.get_fund_intro(fd_code)
 
-    detail = await xqf.get_fund_detail(fd_code)
-    intro = await xqf.get_fund_intro(fd_code)
+fd_name = intro["data"]["fd_name"]
+other_rate_table = detail["data"]["fund_rates"]["other_rate_table"]
 
-    fd_name = intro["data"]["fd_name"]
-    other_rate_table = detail["data"]["fund_rates"]["other_rate_table"]
-
-    print({fd_name: other_rate_table})
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+print({fd_name: other_rate_table})
